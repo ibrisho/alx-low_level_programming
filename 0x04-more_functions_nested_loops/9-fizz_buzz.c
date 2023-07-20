d465cf6ecb5e09c5ea2483bff800ffe1c72a252b
ibra <imorketa9@gmail.com>
2023-07-20 16:00:31 +0000

    complete:all tasks

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Classic FizzBuzz problem
+ * Return: Always 0, Success!
+ */
+int main(void)
+{
+	int i;
+	char result[4][9] = {"%d", "Fizz", "Buzz", "FizzBuzz"};
+
+	for (i = 1; i < 101; i++)
+	{
+		printf(result[(i % 3 == 0) + (2 * (i % 5 == 0))], i);
+		printf(i < 100 ? " " : "\n");
+	}
+	return (0);
+}
