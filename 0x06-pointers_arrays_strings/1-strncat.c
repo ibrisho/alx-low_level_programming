4e878c08633afd14d5c5a504e9b5bacad22386ec
Ibrahim <imorketa9@gmail.com>
2023-07-27 14:59:45 +0000

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates destination string with n bytes from source string
+ * @dest:pointer to destination string
+ * @src:pointer to source string
+ *
+ * Return:pointer to destination string concatenated
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j;
+
+	while (dest[i] != '\0')
+		i++;
+	for (j = 0; j < n && src[j] != '\0'; j++, i++)
+		dest[i] = src[j];
+	dest[i] = '\0';
+	return (dest);
+}
