20a09f4ebfec8405bb034b76893b1e6a65f40396
Ibrahim <imorketa9@gmail.com>
2023-07-31 15:53:44 +0000

    fix chess board

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,13 +6,18 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int j = 1;
+	int x, y;
 
-	while (a[0][j - 1] != 0)
+	x = 0;
+	while (x < 8)
 	{
-		_putchar(a[0][j - 1]);
-		if (j > 1 && j % 8 == 0)
-			_putchar('\n');
-		j++;
+		y = 0;
+		while (y < 8)
+		{
+			_putchar(a[x][y]);
+			y++;
+		}
+		_putchar('\n');
+		x++;
 	}
 }
