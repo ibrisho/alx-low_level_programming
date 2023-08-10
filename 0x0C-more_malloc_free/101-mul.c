4d59966d0820545a45226abaaa32a1b0029d9224
Ibrahim <imorketa9@gmail.com>
2023-08-10 17:13:04 +0000

    add 2 functions to check if inputs are digits

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,38 @@
 
 
 /**
+ * is_digit - checks for digits
+ * @c: input character to check for digit
+ * Return: 0 failure, 1 success
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+/**
+ * isDigitString - checks if string is digit
+ * @str: pointer to string to check
+ *
+ * Return: 1 if digit 0 if not
+ */
+int isDigitString(const char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (!is_digit(str[i]))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+/**
  * main - prints all arguments received.
  * @argc: type int argument
  * @argv: type char argument of string.
@@ -14,7 +46,13 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
+	}
+
+	if (!isDigitString(n1) && !isDigitString(n2))
+	{
+		printf("Error\n");
+		exit(98);
 	}
 	else
 	{
