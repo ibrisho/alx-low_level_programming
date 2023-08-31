aaad9f3c666c5a9399847e60f0b447cbb21fea5f
Ibrahim <imorketa9@gmail.com>
2023-08-31 15:02:36 +0000

    print binary representation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,8 +11,11 @@ void print_binary(unsigned long int n)
 	unsigned int c = 0;
 
 	if (n == 0)
+	{
+		_putchar('0');
 		return;
+	}
 	c = n & 0x01;
 	print_binary(n >> 1);
-	putchar(c + '0');
+	_putchar(c + '0');
 }
