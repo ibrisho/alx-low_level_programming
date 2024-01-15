e1632ab2c737c3283f2c0f3f3e863bee0cc3d1b2
Ibrahim <imorketa9@gmail.com>
2024-01-15 09:05:44 +0000

    fix requirements

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/gigalib.so "https://raw.githubusercontent.com/ibrisha/alx-low_level_programming/main/0x18-dynamic_libraries/gigalib.so"
+wget -q -O /tmp/gigalib.so "https://raw.githubusercontent.com/ibrisho/alx-low_level_programming/main/0x18-dynamic_libraries/gigalib.so"
 export LD_PRELOAD=/tmp/gigalib.so
